f186e344c9a23de96832db41e8cd9911712e42f1
gr00ze <vincenzop23@gmail.com>
2026-08-08 03:19:03 +0200

    Add enabled and radius config

--- src/main/java/com/gr00ze/diagram3D/ClientConfig.java
+++ src/main/java/com/gr00ze/diagram3D/ClientConfig.java
@@ -9,19 +9,30 @@ import net.neoforged.neoforge.common.ModConfigSpec;
 public class ClientConfig
 {
 
-    public static final int UPDATE_REQUEST_INTERVAL = 1;
+    public static final int UPDATE_REQUEST_TICK_INTERVAL = 1;
     public static final long TIMEOUT_RENDER_TICKS = 20;
     public static final double FORCE_VISUAL_SCALE = 0.1;
-    public static final double RADIUS = 20;
+
+    private static final double DEFAULT_RADIUS = 20;
+    private static final boolean DEFAULT_ENABLED = true;
+
+    public static double RADIUS = DEFAULT_RADIUS;
+    public static boolean ENABLED = DEFAULT_ENABLED;
 
     private static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
+
+    private static final ModConfigSpec.DoubleValue RADIUS_CONFIG = BUILDER.defineInRange("radius", DEFAULT_RADIUS, 0, 128);
+    private static final ModConfigSpec.BooleanValue ENABLED_CONFIG = BUILDER.define("enabled", true);
 
     static final ModConfigSpec SPEC = BUILDER.build();
 
     @SubscribeEvent
     static void onLoad(final ModConfigEvent event)
     {
-
+        if (event.getConfig().getSpec() == SPEC) {
+            RADIUS = RADIUS_CONFIG.get();
+            ENABLED = ENABLED_CONFIG.getAsBoolean();
+        }
     }
 
 }
