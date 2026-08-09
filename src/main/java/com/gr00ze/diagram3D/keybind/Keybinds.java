62030860bd0ba4752fd957f60795db855ee621f5
gr00ze <vincenzop23@gmail.com>
2026-08-09 19:38:22 +0200

    Add keybind to enable mod

--- src/main/java/com/gr00ze/diagram3D/keybind/Keybinds.java
+++ src/main/java/com/gr00ze/diagram3D/keybind/Keybinds.java
@@ -1,5 +1,46 @@
 package com.gr00ze.diagram3D.keybind;
 
+import com.gr00ze.diagram3D.ClientConfig;
+import com.gr00ze.diagram3D.Diagram3D;
+import com.mojang.blaze3d.platform.InputConstants;
+import net.minecraft.client.KeyMapping;
+import net.neoforged.neoforge.client.event.ClientTickEvent;
+import net.neoforged.neoforge.client.event.RegisterKeyMappingsEvent;
+import org.lwjgl.glfw.GLFW;
+
 public class Keybinds {
+    public static KeyMapping createMapping(
+        String name,
+        com.mojang.blaze3d.platform.InputConstants.Type type,
+        int keyCode
+    ) {
+        return new KeyMapping(
+            String.format("key.%s.%s", Diagram3D.MOD_ID, name),
+            type,
+            keyCode,
+            String.format("key.categories.%s", Diagram3D.MOD_ID)
+        );
+    }
+
+    public static void register(RegisterKeyMappingsEvent event) {
+        event.register(TOGGLE_KEY);
+    }
+
+    public static final KeyMapping TOGGLE_KEY = createMapping(
+        "toggle",
+        InputConstants.Type.KEYSYM,
+        GLFW.GLFW_KEY_V
+    );
+
+    public static void checkKeys(){
+
+        while (Keybinds.TOGGLE_KEY.consumeClick()) {
+            ClientConfig.ENABLED = !ClientConfig.ENABLED;
+        }
+    }
+
+
+
+
 
 }
