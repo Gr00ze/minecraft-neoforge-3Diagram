89fdfc7e6b59d2aa2435cdbecbcd1a7345097e64
gr00ze <vincenzop23@gmail.com>
2026-08-19 03:01:49 +0200

    Add support class to manage rendering data preparation

---
+++ src/main/java/com/gr00ze/diagram3D/render/RenderPreparation.java
@@ -0,0 +1,49 @@
+package com.gr00ze.diagram3D.render;
+
+import com.gr00ze.diagram3D.config.ForceGroupDisplayConfig;
+import com.gr00ze.diagram3D.data.DiagramRecords;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.phys.Vec3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RenderPreparation {
+    public record InformationDisplay(
+        Vec3 position,
+        List<Component> components
+    ) {}
+
+    public record PreparedForce(
+        Vec3 origin,
+        Vec3 delta,
+        int color,
+        Component name,
+        ForceGroupDisplayConfig config
+    ) {}
+    public record IconDisplay(
+        Vec3 position
+    ) {}
+
+    public record PreparedGroup(
+        DiagramRecords.ResolvedForceGroup forceGroup,
+        ForceGroupDisplayConfig config
+    ) {}
+
+    public record PreparedDiagram(
+        double mass,
+        Vec3 centerOfMass,
+        List<PreparedGroup> groups
+    ) {}
+
+    public record LookingForce(
+        PreparedForce vector,
+        Vec3 position,
+        double rayParameter
+    ) {}
+
+    public final List<PreparedForce> vectors = new ArrayList<>();
+    public final List<InformationDisplay> information = new ArrayList<>();
+    public final List<IconDisplay> icons = new ArrayList<>();
+    public final List<PreparedDiagram> diagrams = new ArrayList<>();
+}
