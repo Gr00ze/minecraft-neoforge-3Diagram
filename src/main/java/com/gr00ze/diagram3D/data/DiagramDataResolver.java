6a9a79a7160aa7575fddc05018554387fa2894b2
gr00ze <vincenzop23@gmail.com>
2026-08-25 15:48:54 +0200

    Add check for invalid sublevelID

--- src/main/java/com/gr00ze/diagram3D/data/DiagramDataResolver.java
+++ src/main/java/com/gr00ze/diagram3D/data/DiagramDataResolver.java
@@ -1,5 +1,6 @@
 package com.gr00ze.diagram3D.data;
 
+import com.gr00ze.libs.ConversionUtils;
 import dev.ryanhcode.sable.api.physics.force.ForceGroup;
 import dev.ryanhcode.sable.api.physics.force.QueuedForceGroup;
 import dev.ryanhcode.sable.api.sublevel.ClientSubLevelContainer;
@@ -13,6 +14,7 @@ import net.minecraft.world.phys.Vec3;
 import org.joml.Vector3d;
 import org.joml.Vector3dc;
 
+import javax.annotation.Nullable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -61,43 +63,31 @@ public class DiagramDataResolver {
 
         return result;
     }
-    public static DiagramRecords.InWorldForce convertSubLevelToLevel(UUID subLevelId, Vector3dc localFrom, Vector3dc localTo)
+    public static DiagramRecords.InWorldForce convertSubLevelToLevel(UUID subLevelId, Vector3dc localOrigin, Vector3dc localDelta)
     {
         Minecraft mc = Minecraft.getInstance();
 
         ClientLevel level = mc.level;
-        if (level == null)
-            return new DiagramRecords.InWorldForce(
-                new Vec3(localFrom.x(), localFrom.y(), localFrom.z()),
-                new Vec3(localTo.x(), localTo.y(), localTo.z())
-            ); //Return the original
+        if (level == null) return null;
 
         ClientSubLevelContainer container = ClientSubLevelContainer.getContainer(level);
 
-        if (container == null)
-            return new DiagramRecords.InWorldForce(
-                new Vec3(localFrom.x(), localFrom.y(), localFrom.z()),
-                new Vec3(localTo.x(), localTo.y(), localTo.z())
-            );  //Return the original
+        if (container == null) return null;
 
         ClientSubLevel subLevel = (ClientSubLevel) container.getSubLevel(subLevelId);
 
-        if (subLevel == null)
-            return new DiagramRecords.InWorldForce(
-                new Vec3(localFrom.x(), localFrom.y(), localFrom.z()),
-                new Vec3(localTo.x(), localTo.y(), localTo.z())
-            );;  //Return the original
+        if (subLevel == null) return null;
 
-
-        Pose3dc pose = subLevel.renderPose(); //try render pose
-        Vector3d from = pose.transformPosition(localFrom, new Vector3d());
-        Vector3d to = pose.transformNormal(localTo, new Vector3d());
-
-
-        return new DiagramRecords.InWorldForce(new Vec3(from.x, from.y, from.z),new Vec3(to.x, to.y, to.z));
+        var globalVector = ConversionUtils.convertSubLevelToLevel(subLevel, localOrigin, localDelta);
+        return new DiagramRecords.InWorldForce(
+            globalVector.origin(),
+            globalVector.delta()
+        );
     }
 
-    public static Vec3 getInWorldMassPosition(UUID completed) {
+
+
+    public static @Nullable Vec3 getInWorldMassPosition(UUID completed) {
         Minecraft mc = Minecraft.getInstance();
 
         ClientLevel level = mc.level;
@@ -113,7 +103,7 @@ public class DiagramDataResolver {
         Vector3d rotationPointLocal = subLevel.logicalPose().rotationPoint();
 
 
-        Pose3dc pose = subLevel.renderPose(); //try render pose
+        Pose3dc pose = subLevel.renderPose();
         Vector3d rotationGlobal = pose.transformPosition(rotationPointLocal, new Vector3d());
 
 
